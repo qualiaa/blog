f5e93b2977a81a64e2918cb9b2c3977a56841f88
Jamie Bayne <jamiebayne@hotmail.com>
2018-07-19 00:55:37 +0200

    Update title and tags for WIP articles

--- json.py
+++ json.py
@@ -1,6 +1,7 @@
 from subprocess import CalledProcessError
 
 from django.http import JsonResponse, Http404, HttpResponseServerError, HttpResponseBadRequest
+from yaml import YAMLError
 
 from . import article
 from . import pandoc
@@ -9,14 +10,14 @@ def wip(request, slug):
     markdown_path = article.WIP_PATH / slug / article.MARKDOWN_FILENAME
     if not markdown_path.exists():
         raise Http404
-    try:
-        html = pandoc.md2html(markdown_path)
-    except FileNotFoundError as e:
-        raise Http404
-    except IOError as e:
-        return HttpResponseServerError("IO Error")
-    except ValueError as e:
-        return HttpResponseServerError("Invalid request")
+
+    try: metadata = article.extract_metadata(markdown_path)
+    except (YAMLError, IOError): metadata = {}
+
+    try: html = pandoc.md2html(markdown_path)
+    except FileNotFoundError as e: raise Http404
+    except IOError as e: return HttpResponseServerError("IO Error")
+    except ValueError as e: return HttpResponseServerError("Invalid request")
     except CalledProcessError as e:
         err_msg = "Error preparing file."
         if settings.DEBUG:
@@ -24,5 +25,7 @@ def wip(request, slug):
         return HttpResponseServerError(err_msg)
 
     html = article.post_processing(html, slug)
+    context = { "html":html }
+    context.update(metadata)
 
-    return JsonResponse({"html":html})
+    return JsonResponse(context)
--- static/blog/js/wip.js
+++ static/blog/js/wip.js
@@ -5,12 +5,20 @@ var requestUrl = "/json/wip/" + slug
 
 $(function() {
     setInterval(function() {
-        console.log("Making request")
-        console.log(requestUrl)
         $.get(requestUrl,
             function(data) {
                 var article_html = data["html"]
-                console.log(article_html)
+                if (data.hasOwnProperty("title")) {
+                    var title = data["title"]
+                    $("h1#title").text(title)
+                }
+                if (data.hasOwnProperty("tags")) {
+                    $("p.tags").text("Tags: "+ data.tags.map(function(s) {
+                            return s[0].toUpperCase() + s.slice(1)
+                        }).join(", "))
+                } else {
+                    $("p.tags").text("Tags: None")
+                }
 
                 $("#article-html").html(article_html)
                 $("#article-html pre code").each(function(i, block) {
