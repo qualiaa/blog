0cd064e57259424b23a0435f18a9dc39dfdf0cd7
Jamie Bayne <jamiebayne@hotmail.com>
2018-07-19 17:45:21 +0200

    Smooth MathJax buffering

--- static/blog/js/wip.js
+++ static/blog/js/wip.js
@@ -1,4 +1,14 @@
 "use strict"
+ 
+var jaxFinishedRendering = true
+window.MathJax = {
+    showProcessingMessages: false,
+    messageStyle: "none",
+    rmessageStyle: "none",
+    "fast-preview": {
+        disabled: true
+    }
+}
 
 var updateTime = 1000
 var requestUrl = "/json/wip/" + slug
@@ -7,7 +17,6 @@ $(function() {
     setInterval(function() {
         $.get(requestUrl,
             function(data) {
-                var article_html = data["html"]
                 if (data.hasOwnProperty("title")) {
                     var title = data["title"]
                     $("h1#title").text(title)
@@ -20,14 +29,78 @@ $(function() {
                     $("p.tags").text("Tags: None")
                 }
 
-                $("#article-html").html(article_html)
-                $("#article-html pre code").each(function(i, block) {
+                if (!jaxFinishedRendering) {
+                    return
+                }
+                var article_html = data["html"]
+                
+                function swapBuffers() {
+                    jaxFinishedRendering = true
+                    var o=$("div#article-html")
+                    var n=$("div#buffer")
+                    o.attr("id","buffer")
+                    o.css("display","none")
+                    n.attr("id","article-html")
+                    n.css("display","inherit")
+                }
+
+                var buffer = $("#buffer")
+                if (buffer.length == 0) {
+                    buffer = $("<div id='buffer' style='display:none;'></div>")
+                    buffer.insertAfter("#article-html")
+                    
+                }
+                buffer.html(article_html)
+                buffer.find("pre code").each(function(i, block) {
                     hljs.highlightBlock(block)
                 })
-                // MathJax.Hub.Queue(["Typeset",MathJax.Hub])
+                jaxFinishedRendering = false
+                MathJax.Hub.Queue(["Typeset",MathJax.Hub,buffer[0],swapBuffers])
+
+
+
             }).fail(function(jqXHR, textStatus, errorThrown) {
                 console.log("Failure\n"+textStatus + "\n" + errorThrown)
                 console.log(jqXHR.responseText)
             })
     }, updateTime)
 })
+
+/*
+ * MathJax attempts that didn't work:
+ */
+    /*
+     * Attempt to process MathJax script elements and add the
+     * processed elements to the page
+     *
+    var article_html = data["html"]
+    var wrapper_object = $("<div>").append($(article_html))
+
+    wrapper_object.find("script[type='math/tex']").html(function() {
+        console.log(MathJax.HTML.Element("div",null,this.outerHTML).outerHTML)
+        return MathJax.HTML.Element("div",null,this.outerHTML).outerHTML
+    })
+    $("#article-html").html(wrapper_object.html())
+    */
+    
+    /*
+     * Attempt to typeset the whole dom fragment before adding to
+     * the page
+     *
+    var article_html = data["html"]
+    var wrapper_object = $("<div>").append($(article_html))
+    var domFragment = wrapper_object[0]
+
+    console.log("MathJaxing")
+    console.log(domFragment)
+    var wrapper_object = $("<div>").append($(article_html))
+    var domFragment = wrapper_object[0]
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub, domFragment, function(x) {
+        console.log(domFragment === wrapper_object[0])
+        console.log(domFragment)
+        $("#article-html").html(wrapper_object.html())
+        $("#article-html pre code").each(function(i, block) {
+            hljs.highlightBlock(block)
+        })
+    }])
+    */
