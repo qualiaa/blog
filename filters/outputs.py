caeec755df24ca28c01f1895e416d709b4e00a54
Jamie Bayne <jamiebayne@hotmail.com>
2018-07-25 02:49:40 +0200

    Add JSON and RequestContext output filters

--- filters/outputs.py
+++ filters/outputs.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+from django.template import RequestContext
 from django.shortcuts import render
 
 from .base import CheckedFilter
@@ -9,3 +11,15 @@ class Render(CheckedFilter):
 
     def __call__(self, request, context):
         return render(request, self.template, context)
+
+class JSON(CheckedFilter):
+    def __init__(self, inputs={}):
+        super().__init__(inputs=inputs)
+
+    def __call__(self, request, context):
+        return JsonResponse(context)
+
+class ToRequestContext(CheckedFilter):
+    def __call__(self, request, context):
+        return RequestContext(request, context)
+
