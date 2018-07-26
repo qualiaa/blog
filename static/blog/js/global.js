078d542f763178c488a582dc949d13de4abe78ec
Jamie Bayne <jamiebayne@hotmail.com>
2018-07-26 15:13:51 +0200

    Style section links to be less obtrusive

--- static/blog/js/global.js
+++ static/blog/js/global.js
@@ -2,21 +2,23 @@
 
 function addPermalinkToSections(jObj) {
     jObj.find("h1, h2, h3, h4, h5").each(function(i,obj) {
-        var id_attr = $(this).attr("id") 
+        var id_attr = $(this).attr("id");
         if (typeof id_attr !== typeof undefined && id_attr != false) {
-            $(this).text(" "+$(this).text())
-            var permalink = $("<a>§</a>").attr("href","#"+id_attr)
-            $(this).prepend(permalink)
+            $(this).text(" "+$(this).text());
+            var permalink = $("<a>§</a>")
+                .attr("href","#"+id_attr)
+                .addClass("section-link");
+            $(this).prepend(permalink);
         }
-    })
+    });
 }
 
 function highlightCode(jObj) {
     jObj.find("pre code").each(function(i, block) {
         /* Need to move pandoc's <pre> classes to <code> tag for hljs */
-        $(this).attr("class", $(this).parent().attr("class"))
-        hljs.highlightBlock(block)
+        $(this).attr("class", $(this).parent().attr("class"));
+        hljs.highlightBlock(block);
     })
 }
 
-$(function() { highlightCode($("article")) })
+$(function() { highlightCode($("article")) });
