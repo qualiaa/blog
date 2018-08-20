12dc456315013d8e9c75d7f0c262e009fe97ceca
Jamie Bayne <jamiebayne@hotmail.com>
2018-08-20 18:19:37 +0200

    AJAX tag calculations

--- ajax.py
+++ ajax.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponseNotModified
+from django.http import HttpResponseNotModified, HttpResponseBadRequest
 
 from .filters import article as a
 from .filters import errors as e
-from .filters.flow import Either
+from .filters.flow import Either, For
 from .filters.inputs import ContextInput
 from .filters.outputs import JSON, ToRequestContext
 from .filters.postprocessing import postprocessing
-from .filters.utils import Extract, Remove
+from .filters.utils import Extract, Remove, Lambda
+from .filters.Paginate import Paginate
+from .filters.Tags import Tags
 
 from . import settings as s
 
@@ -24,4 +26,30 @@ def wip(request, slug):
                    e.NotFound) |\
             Extract("article")  |\
             Remove({"path","markdown"}) |\
+            JSON()
+
+def tags(request):
+    try:
+        tags = request.POST["tags"]
+        page = request.POST["page"]
+    except KeyError:
+        return HttpResponseBadRequest("Require tags and page in POST")
+
+    @Lambda
+    def article_error(r, c):
+        c["article"]["html"] = "Could not load article"
+        return r, c
+
+    return Tags(tags)                                               |\
+            Paginate(page=page, items_per_page=s.ARTICLES_PER_PAGE) |\
+            For(over="paths", to="path", giving="article", result="article_list",
+                    f=a.DateAndSlugFromPath() | 
+                        a.MetadataSafe() |
+                        Alternative(a.MetadataDangerous(), article_error) |
+                        Alternative(c.CachedText(stub=True),
+                            Alternative(a.GetStub() |
+                                    postprocessing() |
+                                    c.CacheHTML(stub=True),
+                                article_error)) |
+                        Remove({"markdown","path"}))                |\
             JSON()
--- urls.py
+++ urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("tags/<str:tag_string>", views.tags_view, name="tags"),
     path("tags/<str:tag_string>/page/<int:page>", views.tags_view, name="tags-page"),
     path("json/wip/<slug:slug>", ajax.wip),
+    path("json/tags", ajax.tags),
     path("<slug:slug>", views.article_view, name="article"),
     path("<slug:slug>/<path:url>", views.article_media),
     path("<slug:slug>.md", views.md),
