e67d1ad85e93c009ddfc118cf82b24c07d455135
Jamie Bayne <jamiebayne@hotmail.com>
2018-08-20 18:15:27 +0200

    Add first steps of TOC generation code

--- static/blog/js/article.js
+++ static/blog/js/article.js
@@ -1,1 +1,21 @@
+function generateTOC(jObj) {
+    function findChildHeaders(jObj, level=1) {
+        console.log(jObj.text())
+        if (level > 5) return;
+        currentLevelTag = "h" + level
+        nextLevelTag = "h" + (level + 1)
+        nextLevelHeadings = jObj.nextUntil(nextLevelTag,currentLevelTag)
+        return findChildHeaders(jObj, level=level+1)
+    }
+
+    for (var i = 1; i <= 5; ++i) {
+        headerTag = "h" + i;
+        headers = jObj.children(headerTag)
+        if (headers.length != 0) {
+            console.log("Found header at level " + i)
+            return findChildHeaders(headers, level=i)
+        }
+    }
+    console.log("Found no headers")
+}
 $(function(){addPermalinkToSections($(".article-html"))})
