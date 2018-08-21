ee5754469e8d5f2d3ff06e3728716ab00d8f8949
Jamie Bayne <jamiebayne@hotmail.com>
2018-08-21 13:26:33 +0200

    Generate HTML TOC from list of header dom elements

--- static/blog/js/article.js
+++ static/blog/js/article.js
@@ -1,31 +1,60 @@
+"use strict"
+
+$.fn.exists = function() {
+    return this.length > 0 ? this : false;
+}
+
 function generateTOC(jObj) {
+    function headerListToHTML(list) {
+        let string = "<ul>";
+        for (let el of list) {
+            if (Array.isArray(el)) {
+                string += headerListToHTML(el);
+            } else {
+                string += `
+                        <li><a href="#${el.id}">${el.textContent}</a></li>
+                    `;
+            }
+        }
+        string += "</ul>";
+        return string;
+    }
+
     function findChildHeaders(level=1) {
         return function (index, domElement) {
-            text = domElement.textContent
-            console.log(domElement.nodeName)
-            console.log(domElement.textContent)
-            if (level > 5) return;
-            currentLevelTag = "h" + level
+            let text = domElement.textContent;
+            let currentLevelTag = "h" + level;
             for (i = level+1; i <= 5; ++i) {
-                nextLevelTag = "h" + i
-                nextLevelHeadings = $(this).nextUntil(currentLevelTag, nextLevelTag)
+                let nextLevelTag = "h" + i;
+                let nextLevelHeadings = $(this).nextUntil(currentLevelTag, nextLevelTag);
                 if (nextLevelHeadings.length > 0) {
-                    return [text, nextLevelHeadings.map(findChildHeaders(i)).get()]
+                    return [domElement, nextLevelHeadings.map(findChildHeaders(i)).get()];
                 }
             }
-            return text
+            return domElement;
         }
     }
 
+    let listOfHeaders = null;
+
     for (var i = 1; i <= 5; ++i) {
-        headerTag = "h" + i;
-        headers = jObj.children(headerTag)
+        let headerTag = "h" + i;
+        let headers = jObj.children(headerTag);
         if (headers.length != 0) {
-            console.log("Found header at level " + i)
-            console.log(headers.map(findChildHeaders(i)).get())
-            return
+            listOfHeaders = headers.map(findChildHeaders(i)).get();
         }
     }
-    console.log("Found no headers")
+
+    if (listOfHeaders !== null) {
+        ($("#toc").exists() ||
+            $("<aside>").
+                addClass("sidebar").
+                prop("id","toc").
+                appendTo("#side")
+            ).html(`<h2>Contents</h2>
+                ${headerListToHTML(listOfHeaders)}`
+            );
+    }
+
 }
-$(function(){addPermalinkToSections($(".article-html"))})
+$(function(){addPermalinkToSections($(".article-html"))});
