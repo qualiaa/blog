24a7c74acbbc241018504fee33da7bb5a2bfdfd6
Jamie Bayne <jamiebayne@hotmail.com>
2018-09-26 22:44:10 +0200

    Cache WIP articles

--- ajax.py
+++ ajax.py
@@ -2,7 +2,8 @@ from django.http import HttpResponseNotModified
 
 from .filters import article as a
 from .filters import errors as e
-from .filters.flow import Either
+from .filters import cache as c
+from .filters.flow import Either, Alternative
 from .filters.inputs import ContextInput
 from .filters.outputs import JSON, ToRequestContext
 from .filters.postprocessing import postprocessing
@@ -14,14 +15,18 @@ def wip(request, slug):
     path = s.WIP_PATH / slug 
     markdown_path = path / s.MARKDOWN_FILENAME
     local_mtime = markdown_path.stat().st_mtime 
+    print("Server mtime:",local_mtime)
     if "mtime" in request.POST:
+        print("Client mtime:",request.POST["mtime"])
         if int(local_mtime) <= float(request.POST["mtime"]):
             return HttpResponseNotModified()
 
     return ContextInput(request, path=path, slug=slug, mtime=local_mtime) >\
             a.MetadataSafe()   |\
-            Either(a.MetadataDangerous() | a.GetFullText() | postprocessing(),
-                   e.NotFound) |\
+            Alternative(c.CachedText(),
+                Either(a.MetadataDangerous() | a.GetFullText() |
+                    postprocessing() | c.CacheHTML(),
+                       e.NotFound)) |\
             Extract("article")  |\
             Remove({"path","markdown"}) |\
             JSON()
