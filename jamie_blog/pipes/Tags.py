34da9d004153b76fe42e87c0d9612c0296c82dd1
Jamie Bayne <jamiebayne@hotmail.com>
2021-04-29 14:18:42 +0100

    Add Tag class

--- jamie_blog/pipes/Tags.py
+++ jamie_blog/pipes/Tags.py
@@ -4,19 +4,19 @@ from itertools import chain
 from django.http import Http404
 
 from .base import CheckedPipe
-from ..tag import get_articles_for_tag
+from ..tag import Tag
 
 class Tags(CheckedPipe):
-    def __init__(self, tags : [str]):
-        super().__init__(outputs={"paths","tags","title"})
-        self.tags = tags
+    def __init__(self, tag_names: [str]):
+        super().__init__(outputs={"paths", "tags", "title"})
+        self.tags = [Tag(t) for t in tag_names]
 
         # TODO raise sensible error
         if not all(re.match("[A-Za-z_]",x) for x in self.tags):
             raise ValueError("Invalid tag in {}".format(self.tags))
 
     def __call__(self, request, context):
-        article_paths = chain(*[get_articles_for_tag(t) for t in self.tags])
+        article_paths = chain(*[t.article_paths for t in self.tags])
         article_paths = set(article_paths)
 
         if "paths" in context:
--- jamie_blog/tag.py
+++ jamie_blog/tag.py
@@ -1,5 +1,6 @@
 import logging
 from os.path import relpath
+from pathlib import Path
 
 from django.conf import settings as s
 
@@ -9,25 +10,59 @@ else:
     from . import article
 
 
-def get_articles_for_tag(tag):
-    return [x.resolve() for x in (s.BLOG_TAG_PATH/tag).iterdir()]
+class Tag:
+    def __init__(self, name: str, *, create=False):
+        self._machine_name = self._munge(name)
+
+        if not self.folder.exists():
+            if create:
+                self._create()
+            else:
+                raise FileNotFoundError(f"No folder {self.folder} for "
+                                        f"{self.pretty_name}")
+
+    @property
+    def pretty_name(self) -> str:
+        return self._unmunge(self.machine_name)
+
+    @property
+    def machine_name(self) -> str:
+        return self._machine_name
+
+    @property
+    def article_paths(self):
+        yield from map(Path.resolve, self.folder.iterdir())
+
+    @property
+    def folder(self) -> Path:
+        return s.BLOG_TAG_PATH/self.machine_name
+
+    def add_article(self, article_folder: Path):
+        # TODO: Warn on nonexistent tags
+        symlink = self.folder/article_folder.name
+        article_relative_to_tag_folder = relpath(article_folder, self.folder)
+        symlink.symlink_to(article_relative_to_tag_folder)
+
+    def _create(self):
+        self.folder.mkdir(mode=0o700, exist_ok=True, parents=True)
+
+    def clear_articles(self):
+        for article_link in self.article_paths:
+            article_link.unlink()
+
+    @staticmethod
+    def _munge(name: str) -> str:
+        return name.lower().replace(" ", "_")
+
+    @staticmethod
+    def _unmunge(name: str) -> str:
+        return name.title().replace("_", " ")
 
 
 def _clear_existing_tags():
     for tag_folder in s.BLOG_TAG_PATH.iterdir():
         for tagged_article in tag_folder.iterdir():
             tagged_article.unlink()
-
-
-def add_article_to_tag(article_folder, tag):
-    # TODO: Warn on nonexistent tags
-    tag_folder = s.BLOG_TAG_PATH/tag
-    symlink = tag_folder/article_folder.name
-    article_relative_to_tag_folder = relpath(article_folder, tag_folder)
-    if tag_folder.exists():
-        symlink.symlink_to(article_relative_to_tag_folder)
-    else:
-        raise FileNotFoundError(f"No folder {tag_folder} for {tag}")
 
 
 def tag_article(folder_path):
@@ -37,11 +72,11 @@ def tag_article(folder_path):
         logging.error("Could not tag article: %s", e.args[0])
         return
     metadata = article.extract_metadata(path)
-    tags = metadata.get("tags", [])
-    if not tags:
+    tag_names = metadata.get("tags", [])
+    if not tag_names:
         logging.warning("No tags for article: %s", path)
-    for tag in tags:
-        add_article_to_tag(folder_path, tag)
+    for tag in map(Tag, tag_names):
+        tag.add_article(folder_path)
 
 
 def init_tags():
@@ -51,15 +86,12 @@ def init_tags():
         AttributeError if settings.BLOG_TAGS not set
     """
     for tag in s.BLOG_TAGS:
-        tag = tag.lower().replace(" ", "_")
-        (s.BLOG_TAG_PATH/tag).mkdir(mode=0o700, exist_ok=True, parents=True)
+        Tag(tag, create=True)
 
 
 def tag_all():
     _clear_existing_tags()
-
-    paths = list(article.get_article_paths())
-    for folder_path in paths:
+    for folder_path in article.get_article_paths():
         tag_article(folder_path)
 
 
