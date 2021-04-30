d84b8a5d3947e38791349ec90c179702474648ce
Jamie Bayne <jamiebayne@hotmail.com>
2021-04-30 18:23:18 +0100

    Fix local Links as well as Images. Handle references

--- jamie_blog/pandoc_filters/20-org-fix-links.lua
+++ jamie_blog/pandoc_filters/20-org-fix-links.lua
@@ -1,6 +1,20 @@
-function Image(elem)
-  if elem.src:match("^./") then
-      elem.src = "LOCAL/" .. elem.src:sub(3)
+function resolve_local_link(link)
+  if link:match("^#") then
+    return "LOCAL" .. link
+  elseif link:match("^./") then
+    return "LOCAL/" .. link:sub(3)
+  elseif link[1] ~= "/" and not link:match("://") then
+    return "LOCAL/" .. link
   end
+  return link
+end
+
+function Image(elem)
+  elem.src = resolve_local_link(elem.src)
+  return elem
+end
+
+function Link(elem)
+  elem.target = resolve_local_link(elem.target)
   return elem
 end
